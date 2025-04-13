8ac74e8841fd5521241af84198db7447bbc9ba1d
Sarath8943 <151513306+Sarath8943@users.noreply.github.com>
2025-04-13 21:07:45 +0530

    commit

---
+++ server/models/reviewModel.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+
+
+const reviewSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+  
+ 
+  restaurantId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Restaurant", // Assuming you have a Restaurant model to reference.
+    required: true 
+  },
+  rating: { 
+    type: Number, 
+    min: 1, 
+    max: 5, 
+    required: true 
+  },
+  comment: { 
+    type: String, 
+    required: true 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+  username: { 
+    type: String, 
+    required: true
+   },
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const MenuItem = require('./menuItemModel');
+
+// const reviewSchema = new mongoose.Schema({
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//   menuItems: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "MenuItem",
+//     // required: true,
+//   },
+//   rating: { type: Number, min: 1, max: 5, required: true },
+//   comment: { type: String, required: true },
+//   createdAt: { type: Date, default: Date.now },
+// });
+
+// const Review = mongoose.model("Review", reviewSchema);
+// module.exports = Review;
+
+
