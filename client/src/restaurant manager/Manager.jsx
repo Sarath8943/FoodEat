6690c9c98c6df5c82c8168b5cee0f038b6135642
Sarath8943 <151513306+Sarath8943@users.noreply.github.com>
2025-05-05 19:33:41 +0530

    c

--- client/src/restaurant manager/Manager.jsx
+++ client/src/restaurant manager/Manager.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Manager from "../restaurant-manager/manager";
 import { useLocation, useNavigate, Routes, Route, Navigate } from 'react-router-dom';
 import { ArrowUpRight, DollarSign, Users } from 'lucide-react';
 
