0e9e367bf44c59f097f18f4151757aa920287c4c
Sarath8943 <151513306+Sarath8943@users.noreply.github.com>
2025-05-05 19:37:53 +0530

    com

--- client/src/Router/router.jsx
+++ client/src/Router/router.jsx
@@ -20,7 +20,6 @@ import Invoice from "../pages/User/CheckoutPage/Invoice";
 import LandingPage from "../pages/shared/LandingPage";
 import ReviewPage from "../pages/User/ReviewPage";
 import CastamerReview from "../pages/User/CastamerReview";
-import Manager from "../restaurant manager/manager";
 
 const router = createBrowserRouter([
   {
