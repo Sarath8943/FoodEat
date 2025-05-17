9eb0e8960334d7a95d2af861d4b022328a6b1761
Sarath8943 <151513306+Sarath8943@users.noreply.github.com>
2025-05-17 19:55:11 +0530

    new

--- client/src/components/user/Footer.jsx
+++ client/src/components/user/Footer.jsx
@@ -126,7 +126,7 @@ const Footer = () => {
                 {[
                   { label: "Home", href: "/" },
                   { label: "About Us", href: "/about" },
-                  { label: "Restaurant Add", href: "/dashbord" },
+                  { label: "Restaurant add ", href: "/dashbord" },
                   { label: "Contact", href: "/contact" },
                   { label: "Privacy Policy", href: "/privacy" },
                   { label: "Terms of Service", href: "/terms" },
